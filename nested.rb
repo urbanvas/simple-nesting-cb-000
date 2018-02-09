cdf43df3e5b790f8213d26ad5c8375c14f6d5806
Neil Seejoor <nvseejoor@gmail.com>
2018-02-09 03:04:44 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -5,7 +5,7 @@ def hopper
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
-        # },
+         }
         # :alan_kay => {
         #   :known_for => "Object Orientation",
         #   :languages => ["Smalltalk", "LISP"]
